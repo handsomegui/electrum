b35f7a7229f1304604ea8f8b4c3ee9c884569ce9
Calin Culianu <calin.culianu@gmail.com>
2018-05-24 21:09:50 +0300

    more tweaks to __main__.py to get SSL to behave.  It's still a hack, tho. TODO: investigate why bitcoincash.com fails verification

--- ios/ElectronCash/__main__.py
+++ ios/ElectronCash/__main__.py
@@ -3,19 +3,26 @@
 # 'applicationDidFinishLaunchingWithOptions' call, which is really where we start the app.
 import electroncash_gui.ios_native.appdelegate
 from electroncash_gui.ios_native.uikit_bindings import *
-import sys, os, ssl
+import sys, ssl
+
 
 if __name__ == '__main__':
-    #
-    # The below is very important to allow OpenSSL to do SSL connections on iOS without verifying certs.
-    # If you take this out, blockchain_headers from http://bitcoincash.com will fail, and the
-    # "downloading headers" thing will take ages.  So I left this in.
-    # TODO: figure out how else to get the certs file into the libOpenSSl.a archive.
-    #   - Calin May 24, 2018
-    #
-    if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None)): 
-        ssl._create_default_https_context = ssl._create_unverified_context
-        print("*** SSL *** Allow Unverfied Context: ENABLED! ;)")
+    try:
+        #
+        # The below is very important to allow OpenSSL to do SSL connections on iOS without verifying certs.
+        # If you take this out, blockchain_headers from http://bitcoincash.com will fail, and the
+        # "downloading headers" thing will take ages.  So I left this in.
+        # TODO: Figure out how to get bitcoincash.com to not fail with cert verification.
+        #   - Calin May 24, 2018
+        #
+        if (getattr(ssl, '_create_unverified_context', None)): 
+            ssl._create_default_https_context = ssl._create_unverified_context
+            ssl.create_default_context = ssl._create_unverified_context
+            print("*** SSL *** Allow Unverfied Context: ENABLED! ;)")
+        else:
+            raise Exception("pyOpenSSL seems to be missing the '_create_unverified_context' function") 
+    except:
+        print("*** SSL *** Allow Unverified Context: FAILED (%s)"%(str(sys.exc_info()[1])))
     
     argc = c_int(len(sys.argv))
     argv = (c_char_p * (argc.value + 1))()
@@ -26,4 +33,4 @@ if __name__ == '__main__':
     #.argtypes = [c_int, POINTER(c_char_p), c_void_p, c_void_p]
     uikit.UIApplicationMain(argc, argv, None, ns_from_py("PythonAppDelegate").ptr)
     sys.exit(0) # ensure we don't end up back in obj-c
-    # at this point process exits.. note that objc's auto-generated main.m invocation of UIApplicationMain will not end up being called!
+    # at this point process exits.. note that the Briefcase auto-generated main.m invocation of UIApplicationMain will not end up being called!
